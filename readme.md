43a915b84f56e54f05d07edcdda66ead14b86bb3
KrazyKirby99999 <isc.bev@gmail.com>
2020-12-26 19:09:43 -0600

    Create initial project structure

---
+++ readme.md
@@ -0,0 +1,2 @@
+#python-cryptography-fernet-wrapper
+[Github Repository](https://github.com/KrazyKirby99999/python-cryptography-fernet-wrapper)
---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="python-cryptography-fernet-wrapper",
+    version="0.0.1",
+    author="krazykirby99999",
+    description="A small example package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/KrazyKirby99999/python-cryptography-fernet-wrapper",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GPLv3 License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
