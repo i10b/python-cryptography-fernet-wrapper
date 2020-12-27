0093cc3de623ef5e0b04d478adea4793ccae7c79
KrazyKirby99999 <isc.bev@gmail.com>
2020-12-26 19:28:19 -0600

    Add generation of keys

---
+++ wrapper/main.py
@@ -0,0 +1,28 @@
+import base64
+import os
+from cryptography.fernet import Fernet
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+
+
+class Wrapper:
+    def __init__(self):
+        pass
+
+    def gen_key(self):
+        key = Fernet.generate_key()
+        return key
+
+    def key_from_pass(self, password):
+        salt = os.urandom(16)
+        kdf = PBKDF2HMAC(
+            algorithm=hashes.SHA256(),
+            length=32,
+            salt=salt,
+            iterations=100000,
+            backend=default_backend()
+        )
+        key = base64.urlsafe_b64encode(kdf.derive(password))
+        return key
+
